70753eeacb62eb0a6a7ef0e5be878c392e62d836
Mahendran GK <mahendran@rigaps.com>
2017-03-09 13:21:44 +0530

    web starter v1.0.0

---
+++ WEB_STARTER/gulpfile.js
@@ -0,0 +1,79 @@
+var gulp = require('gulp');
+var nunjucksRender = require('gulp-nunjucks-render');
+
+var prettify = require('gulp-prettify');
+var notify = require('gulp-notify');
+
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+
+var connect = require('gulp-connect');
+
+var cache = require('gulp-cached');
+
+var autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('resources/views/pages/**/*.+(html|njk)')
+
+  // Changed files
+  //.pipe(cache('public'))
+
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['resources/views/templates']
+    }))
+
+  // prettify html
+  .pipe(prettify({indent_size: 2}))
+
+  // output files in app folder
+  .pipe(gulp.dest('public'))
+
+   // notify the changes
+   .pipe(notify({ message: "Nunjucks Completed!", onLast: true }));
+
+});
+
+gulp.task('sass', function () {
+  return gulp.src('resources/assets/sass/**/*.scss')
+
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+			browsers: ['last 45 versions'],
+			cascade: false
+		}))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('public/Content/Styles'))
+    .pipe(notify({ message: "Sass completed!", onLast: true }));
+});
+
+
+gulp.task('watch', function () {
+
+   gulp.watch([
+     'resources/views/pages/*.html',
+     'resources/views/pages/**/*.html',
+     'resources/views/templates/layouts/*.njk',
+     'resources/views/templates/partials/*.njk',
+     'resources/views/templates/partials/**/*.njk',
+     'resources/views/templates/macros/*.njk',
+     'resources/views/templates/macros/**/*.njk'
+   ], ['nunjucks']);
+
+   gulp.watch([
+     'resources/assets/sass/**/*.scss'
+   ], ['sass']);
+
+
+});
+
+gulp.task('webserver', function() {
+  connect.server({
+    root: 'public'
+  });
+});
+
+gulp.task('default', ['nunjucks', 'sass', 'webserver', 'watch']);
